aa437fcedf020dd3dbc448d0991749c608f5e128
sussica <sussicaya@gmail.com>
2019-05-02 07:02:11 -0700

    Init Commit: finished basic set up

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const port = 3000
+
+app.set('view engine', 'ejs');
+
+
+app.get("/",function(req, res){
+
+
+})
