d4ba38e44ee80c880b02ea62e310871ca869403b
sussica <sussicaya@gmail.com>
2019-05-03 10:44:41 -0700

    Added about page for practicing EJS layout

--- app.js
+++ app.js
@@ -4,12 +4,18 @@ const bodyParser = require('body-parser')
 const port = 3000
 
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"))
 
-
+// To Do List Items
 
 var items = ["EJS Project", "Blog Website", "Algorithm"];
+var workItems = [];
+
+
+// Main Page
 
 
 app.get("/", function(req, res) {
@@ -27,17 +33,41 @@ app.get("/", function(req, res) {
 
   res.render('index', {
     KindOfDay: date,
-    newItems:items
+    newItems: items
   });
-
-
 })
 
 app.post('/', function(req, res) {
-  var item = req.body.newItem
+  let item = req.body.newItem
+  if(req.body.list==='Work List'){
+    workItems.push(item);
+    res.redirect('/work');
+  }else{
   items.push(item);
   res.redirect('/');
+}});
+
+
+
+// Work Page
+
+app.get('/work', function(req, res){
+  res.render('index', {
+    KindOfDay: "Work List",
+    newItems: workItems
+  });
 });
+
+
+app.get('/about', function(req,res){
+  res.render('about');
+})
+
+
+
+
+
+
 
 
 app.listen(port, function() {
