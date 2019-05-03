e1879910be16345c74d78726a2eed100ecc3d174
sussica <sussicaya@gmail.com>
2019-05-03 09:16:01 -0700

    Finished basic feature of my to do list

--- app.js
+++ app.js
@@ -4,9 +4,39 @@ const bodyParser = require('body-parser')
 const port = 3000
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
+var items = ["EJS Project", "Blog Website", "Algorithm"];
 
 
-app.get("/",function(req, res){
+app.get("/", function(req, res) {
+
+  var today = new Date();
+
+  var options = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  };
+
+  var date = today.toLocaleDateString("en-US", options);
+
+  res.render('index', {
+    KindOfDay: date,
+    newItems:items
+  });
 
 
+})
+
+app.post('/', function(req, res) {
+  var item = req.body.newItem
+  items.push(item);
+  res.redirect('/');
+});
+
+
+app.listen(port, function() {
+  console.log("It's running XD")
 })
